f782afea3645cd3c45873fe804f2a2953d47aab8
R4ja34 <raja.lalaina@gmail.com>
2023-11-09 18:56:21 +0100

    setup attendance

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,32 +1,14 @@
+# attendances_controller.rb
 class AttendancesController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    # Logique pour afficher la liste des participations (si nécessaire)
   end
-
-
-  # def new
-  #   @event = Event.find(params[:event_id])
-  #   @attendance = Attendance.new
-  # end
-
-  # def create
-  #   @event = Event.find(params[:event_id])
-  #   @attendance = @event.attendances.build(user: current_user)
-
-  #   # Ajoute le code pour gérer le paiement avec Stripe ici
-  #   # Assure-toi que le paiement est réussi avant de créer la participation
-
-  #   if @attendance.save
-  #     redirect_to @event, notice: 'Inscription réussie!'
-  #   else
-  #     render :new
-  #   end
-  # end
-
   def show
+    @attendance = Attendance.find(params[:id])
   end
-
   def edit
+    @attendance = Attendance.find(params[:id])
   end
 end
--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,6 +1,7 @@
 class CheckoutController < ApplicationController
   def create
     @total = params[:total].to_d
+    @event_id = params[:event_id]
     @session = Stripe::Checkout::Session.create(
       payment_method_types: ['card'],
       line_items: [
@@ -15,7 +16,11 @@ class CheckoutController < ApplicationController
           quantity: 1
         },
       ],
+      metadata: {
+        event_id: @event_id
+      },
       mode: 'payment',
+      customer_email: current_user.email,
       success_url: checkout_success_url + '?session_id={CHECKOUT_SESSION_ID}',
       cancel_url: checkout_cancel_url
     )
@@ -25,7 +30,19 @@ class CheckoutController < ApplicationController
   def success
     @session = Stripe::Checkout::Session.retrieve(params[:session_id])
     @payment_intent = Stripe::PaymentIntent.retrieve(@session.payment_intent)
+    @user = current_user
+    @event_id = @session.metadata.event_id
+    @stripe_id = @session.id
+
+    Attendance.create(
+      event_id: @event_id,
+      user_id: @user.id,
+      stripe_id: @stripe_id
+    )
+
+
   end
+  
   def cancel
   end
 end
